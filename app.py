d9ebfbb5d504c03d9a49c41706c1466b1e9029e8
Arvind Anand <arvindanand1123@gmail.com>
2019-03-02 17:56:17 -0500

    Test App for Predict

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-import flask
-import requests
+from flask import Flask
+from flask import request
+import os
 
-def getPrediction(request):
-    
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Welcome to Prediction'
+
+@app.route('/getPrediction', methods=['POST'])
+def getPrediction():
+    if request.method == 'POST':
+        f = request.form
+        return f
+    else:
+        return "POST Failed"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port = 2222)
